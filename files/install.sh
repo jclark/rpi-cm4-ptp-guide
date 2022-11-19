1455d82138c2eaa163f2644c31956146b5562403
James Clark <jjc@jclark.com>
2022-11-19 21:17:35 +0700

    Do systemctl daemon-reload

--- files/install.sh
+++ files/install.sh
@@ -4,4 +4,6 @@ cp phc2shm.service ts2phc.service ptp4l-gm.service /etc/systemd/system/
 cp shm-refclock.conf /etc/chrony/conf.d/
 cp ptp4l-gm-set.sh /usr/local/sbin/
 cp dhcpcd.exit-hook /etc/
+# reload the service files
+systemctl daemon-reload
 
