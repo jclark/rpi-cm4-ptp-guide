db7873d4119a63033a28e79e7ad47244dca27efe
James Clark <jjc@jclark.com>
2022-11-19 21:03:11 +0700

    Add script to install files

---
+++ files/install.sh
@@ -0,0 +1,7 @@
+# copy config files into place
+cp ptp4l-gm /etc/default/
+cp phc2shm.service ts2phc.service ptp4l-gm.service /etc/systemd/system/
+cp shm-refclock.conf /etc/chrony/conf.d/
+cp ptp4l-gm-set.sh /usr/local/sbin/
+cp dhcpcd.exit-hook /etc/
+
