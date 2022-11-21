4a53fb2a1c2ca554766bf19c6635ae35751b97fa
James Clark <jjc@jclark.com>
2022-11-21 10:52:13 +0700

    Add list of config files

--- files/install.sh
+++ files/install.sh
@@ -1,9 +1,4 @@
 # copy config files into place
-cp ptp4l-gm /etc/default/
-cp phc2shm.service ts2phc.service ptp4l-gm.service /etc/systemd/system/
-cp shm-refclock.conf /etc/chrony/conf.d/
-cp ptp4l-gm-set.sh /usr/local/sbin/
-cp dhcpcd.exit-hook /etc/
+for f in `cat files.txt`; do cp `basename $f` $f; done
 # reload the service files
 systemctl daemon-reload
-
