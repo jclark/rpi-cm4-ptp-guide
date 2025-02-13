79eec480c1b2cec42922001436bd88756b0c4c9c
James Clark <jjc@jclark.com>
2025-02-13 12:16:23 +0700

    Add NetworkManager dispatcher script
    Makes it work on Debian Bookworm

--- files/gm-install.sh
+++ files/gm-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # copy config files into place
-for f in `cat gm-files.txt`; do cp `basename $f` $f; done
+for f in `cat gm-files.txt`; do cp --preserve=mode `basename $f` $f; done
 # reload the service files
 systemctl daemon-reload
